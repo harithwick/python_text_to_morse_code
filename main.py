9cd8b2bd402b41072a88737628cc8955e5cb2e0a
harithwick <harithsen@gmail.com>
2021-08-04 20:55:40 +1000

    minor changes

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 
 from banner import Banner
 from morse_code import morseCode
-print(Banner.text)
 
-inputText = input("Input text to convert to Morse Code: ")
 
-print("\nMorse Code:")
-for character in inputText:
-    print("\t" + morseCode[character.upper()])
-print("\n")
+def to_morse():
+    print(Banner.text)
+    inputText = input("Input text to convert to Morse Code: ")
+    print("\nMorse Code:")
+    for character in inputText:
+        print("\t" + morseCode[character.upper()])
+    print("\n")
+
+
+to_morse()
